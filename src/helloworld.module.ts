bf5247c36a98ff9fea5d2d6eaabdbbb556feaadd
Siewdass <siewdass@gmail.com>
2023-04-15 23:01:07 -0400

    test

---
+++ src/helloworld.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { X } from './helloworld/helloworld.component'
+
+@NgModule({
+  declarations: [
+    X
+  ],
+  imports: [
+
+  ],
+  providers: [
+
+  ]
+})
+export class HelloWorldModule { }
